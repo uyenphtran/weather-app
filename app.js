84b48de2076a984d8c4ea6f28d06bee8404ba2cc
Uyen Tran <puieztao@gmail.com>
2023-02-09 20:42:37 -0800

    Updated icons and styles.css

--- app.js
+++ app.js
@@ -12,13 +12,17 @@ const iconElement = document.querySelector(".icon");
 const tempElement = document.querySelector(".temp p");
 const tempDescElement = document.querySelector(".temp-description p");
 const locElement = document.querySelector(".location p");
+const tempMaxElement = document.querySelector(".temp-max p");
+const tempMinElement = document.querySelector(".temp-min p");
+
+const KELVIN = 273;
+const key = "82005d27a116c2880c8f0fcb866998a0";
 
 const weather = {
     temp : {
         value : 24,
         unit : "celsius"
     },
-    /*
     tempMax : {
         value : 24,
         unit : "celsius"
@@ -27,21 +31,55 @@ const weather = {
         value : 24,
         unit : "celsius"
     },
-     */
-    desc : "Sunny",
-    iconId : "clouds_sun_sunny_weather",
-    city : "San Diego"
+    iconID : "default"
 };
+
+// Check to see if the browser supports geolocation
+if('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(getPositon, displayError);
+}
+else {
+    notifElement.style.display = "block";
+    notifElement.innerHTML = "<p>Error: no geolocation found.</p>";
+}
+
+// Get geolocation of the user
+function getPositon(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    getWeather(latitude, longitude);
+}
+
+// Display error message if any
+function displayError(error) {
+    notifElement.style.display = "block";
+    notifElement.innerHTML = `<p>Error: ${error.message}</p>`;
+}
+
+function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    fetch(api).then(function(response) {
+        let data = response.json()
+        return data;
+    }).then(function(data) {
+        weather.temp.value = Math.floor(data.main.temp - KELVIN);
+        weather.tempMax.value = Math.floor(data.main.temp_max - KELVIN);
+        weather.tempMin.value = Math.floor(data.main.temp_min - KELVIN);
+        weather.desc = data.weather[0].description;
+        weather.iconID = data.weather[0].icon;
+        weather.city = data.name.toLowerCase();
+    }).then(function() {
+        display();
+    });
+}
 
 function display() {
     iconElement.innerHTML = `<img src="icons/${weather.iconID}.png"/>`;
-    tempElement.innerHTML = `${weather.temp.value}°<span>C</span>`;
+    tempElement.innerHTML = `${weather.temp.value}°C`;
+    tempMaxElement.innerHTML = `h: ${weather.tempMax.value}°C`;
+    tempMinElement.innerHTML = `l: ${weather.tempMin.value}°C`;
     tempDescElement.innerHTML = weather.desc;
     locElement.innerHTML = `${weather.city}`;
-}
-
-function convertKelvinToCelcius() {
-    weather.temp.value = 300 - 283;
 }
 
 function convertCelciusToFahrenheit(temp){
@@ -55,15 +93,26 @@ tempElement.addEventListener("click", function(){
     // To change the unit to Fahrenheit
     else if(weather.temp.unit === "celsius") {
         let fahrenheitTemp = convertCelciusToFahrenheit(weather.temp.value);
-        tempElement.innerHTML = `${fahrenheitTemp}°<span>F</span>`;
+        tempElement.innerHTML = `${fahrenheitTemp}°F`;
         weather.temp.unit = "fahrenheit";
+
+        let maxFahrenheitTemp = convertCelciusToFahrenheit(weather.tempMax.value);
+        tempMaxElement.innerHTML = `${maxFahrenheitTemp}°F`;
+        weather.tempMax.unit = "fahrenheit";
+
+        let minFahrenheitTemp = convertCelciusToFahrenheit(weather.tempMin.value);
+        tempMinElement.innerHTML = `${minFahrenheitTemp}°F`;
+        weather.tempMin.unit = "fahrenheit";
     }
     // To change the unit to Celcius
     else {
-        tempElement.innerHTML = `${weather.temp.value}°<span>C</span>`;
+        tempElement.innerHTML = `${weather.temp.value}°C`;
         weather.temp.unit = "celsius";
+
+        tempMaxElement.innerHTML = `${weather.tempMax.value}°C`;
+        weather.tempMax.unit = "celsius";
+
+        tempMinElement.innerHTML = `${weather.tempMin.value}°C`;
+        weather.tempMin.unit = "celsius";
     }
 });
-
-
-
